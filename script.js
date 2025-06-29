01fb76e62fee049981603589a1806f3912e33417
CaretParrot <wopow29@gmail.com>
2025-06-29 15:50:54 -0500

    Refactor header structure and navigation across multiple pages; remove courses.html and update course selection in index, about, competitions, and composition-orchestration pages

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+id.setupTree();
+
 window.onload = function () {
     id.setupTree();
     id.year.innerHTML = new Date().getFullYear();
 }
 
+id.courseSelect.onchange = function () {
+    window.location = id.courseSelect.value;
+}
